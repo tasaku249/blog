c286e3b5577b7dd11a5bc7defcd05b70d525f9d5
tasaku249 <tasaku0005@gmail.com>
2023-01-22 11:18:54 +0900

    new

--- category.php
+++ category.php
@@ -4,22 +4,21 @@
 <div class="works-title">Works</div>
 <hr class="title-hr">
 
+<?php
+      $cat = get_the_category();
+      $catname = $cat[0]->cat_name;
+    ?>
 <nav id="navi">
   <ul>
-    <li><a href="<?php echo esc_url(home_url('/category/column/')); ?>">azure</a></li>
-    <li><a href="<?php echo esc_url(home_url('/category/news/')); ?>">illust</a></li>
-    <li><a href="<?php echo esc_url(home_url('/category/hotel/')); ?>">others</a></li>
+    <li><a class=<?php echo (is_category('column')) ? 'click' : 'no-click' ?> href="<?php echo esc_url(home_url('/category/column/')); ?>">azure</a></li>
+    <li><a class=<?php echo (is_category('news')) ? 'click' : 'no-click' ?> href="<?php echo esc_url(home_url('/category/news/')); ?>">illust</a></li>
+    <li><a class=<?php echo (is_category('hotel')) ? 'click' : 'no-click' ?> href="<?php echo esc_url(home_url('/category/hotel/')); ?>">others</a></li>
   </ul>
 </nav>
 
 <div id="container" class="wrapper">
   <main>  
-    <!--<?php
-      $cat = get_the_category();
-      $catname = $cat[0]->cat_name;
-    ?>
-    <h1 class="page-title"><?php echo $catname; ?></h1>-->
-
+    <!--<h1 class="page-title"><?php echo $catname; ?></h1>-->
     <?php
       if (have_posts()):
         get_template_part('loop');
--- loop.php
+++ loop.php
@@ -4,9 +4,7 @@
     <article>
         
         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-        <div class="article-title">
-        <?php the_title(); ?>
-        </div>
+        <div class="article-title"><a href="<?php the_permalink(); ?>" > <?php the_title(); ?></a></div>
         <ul class="meta">
         <li><?php the_time('Y/m/d'); ?></li>
         </ul>
